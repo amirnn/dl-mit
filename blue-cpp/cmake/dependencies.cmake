5d8750f0e47531b66138d41364ec5efd711b88e0
Amir Nourinia <amir.nouri.nia@gmail.com>
2025-03-14 11:13:34 +0100

    download libtroch at configure time

--- blue-cpp/cmake/dependencies.cmake
+++ blue-cpp/cmake/dependencies.cmake
@@ -1,18 +1,18 @@
 # dependencies
 
-# LibTorch Needs patching described in the readme.
-
-# for debug build
-link_directories("C:/Local/pytorch-lib/debug/libtorch/lib/")
-find_package(Torch PATHS "C:/Local/pytorch-lib/debug/libtorch/share/cmake/Torch" REQUIRED)
-
-# for release build
-# link_directories("C:/Local/pytorch-lib/release/libtorch/lib/")
-# find_package(Torch PATHS "C:/Local/pytorch-lib/release/libtorch/share/cmake/Torch" REQUIRED)
-
-# the same
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TORCH_CXX_FLAGS}")
-
+# download libtorch in case needed
+# thanks to https://github.com/pytorch/examples/blob/main/cpp/mnist/CMakeLists.txt
+option(DOWNLOAD_LIBTROCH "Download the LibTorch" ON)
+if (DOWNLOAD_LIBTROCH)
+    message(STATUS "Downloading LibTorch")
+    execute_process(
+            COMMAND python ${CMAKE_CURRENT_SOURCE_DIR}/cmake/python/download_libtorch.py
+            -d ${CMAKE_BINARY_DIR}/libtorch
+            ERROR_VARIABLE DOWNLOAD_ERROR)
+    if (DOWNLOAD_ERROR)
+        message(FATAL_ERROR "Error downloading LibTorch: ${DOWNLOAD_ERROR}")
+    endif()
+endif()
 
 # download mnist in case needed
 # thanks to https://github.com/pytorch/examples/blob/main/cpp/mnist/CMakeLists.txt
@@ -20,10 +20,24 @@ option(DOWNLOAD_MNIST "Download the MNIST dataset from the internet" ON)
 if (DOWNLOAD_MNIST)
     message(STATUS "Downloading MNIST dataset")
     execute_process(
-            COMMAND python ${CMAKE_CURRENT_SOURCE_DIR}/cmake/download_mnist.py
+            COMMAND python ${CMAKE_CURRENT_SOURCE_DIR}/cmake/python/download_mnist.py
             -d ${CMAKE_BINARY_DIR}/data
             ERROR_VARIABLE DOWNLOAD_ERROR)
     if (DOWNLOAD_ERROR)
         message(FATAL_ERROR "Error downloading MNIST dataset: ${DOWNLOAD_ERROR}")
     endif()
 endif()
+
+
+# LibTorch Needs patching described in the readme.
+
+# for debug build
+link_directories("${CMAKE_BINARY_DIR}/libtorch/out/libtorch/lib/")
+find_package(Torch PATHS "${CMAKE_BINARY_DIR}/libtorch/out/libtorch" REQUIRED)
+
+# for release build
+# link_directories("C:/Local/pytorch-lib/release/libtorch/lib/")
+# find_package(Torch PATHS "C:/Local/pytorch-lib/release/libtorch/share/cmake/Torch" REQUIRED)
+
+# the same
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TORCH_CXX_FLAGS}")
---
+++ blue-cpp/cmake/python/download_libtorch.py
@@ -0,0 +1,110 @@
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import zipfile
+import os
+import sys
+import urllib
+import platform
+
+try:
+    from urllib.error import URLError
+    from urllib.request import urlretrieve
+except ImportError:
+    from urllib2 import URLError
+    from urllib import urlretrieve
+
+Resources: dict[str, tuple[str]] = {
+    "Linux": (
+        "https://download.pytorch.org/libtorch/cu126/libtorch-cxx11-abi-shared-with-deps-2.6.0%2Bcu126.zip",
+    ),
+    "Darwin": (
+        "https://download.pytorch.org/libtorch/cpu/libtorch-macos-arm64-2.6.0.zip",
+    ),
+    "Windows": (
+        "https://download.pytorch.org/libtorch/cu126/libtorch-win-shared-with-deps-2.6.0%2Bcu126.zip",
+        "https://download.pytorch.org/libtorch/cu126/libtorch-win-shared-with-deps-debug-2.6.0%2Bcu126.zip",
+    ),
+}
+
+Items: dict[str, tuple[str]] = {
+    "Linux": ("libtorch-cxx11-abi-shared-with-deps-2.6.0+cu126.zip",),
+    "Darwin": ("libtorch-macos-arm64-2.6.0.zip",),
+    "Windows": (
+        "libtorch-win-shared-with-deps-2.6.0+cu126.zip",
+        "libtorch-win-shared-with-deps-debug-2.6.0+cu126.zip",
+    ),
+}
+
+
+def report_download_progress(chunk_number, chunk_size, file_size):
+    if file_size != -1:
+        percent = min(1, (chunk_number * chunk_size) / file_size)
+        bar = "#" * int(64 * percent)
+        sys.stdout.write("\r0% |{:<64}| {}%".format(bar, int(percent * 100)))
+
+
+def download(destination_path, url, quiet):
+    if os.path.exists(destination_path):
+        if not quiet:
+            print("{} already exists, skipping ...".format(destination_path))
+    else:
+        print("Downloading {} ...".format(url))
+        try:
+            hook = None if quiet else report_download_progress
+            urlretrieve(url, destination_path, reporthook=hook)
+        except URLError:
+            raise RuntimeError("Error downloading resource!")
+        finally:
+            if not quiet:
+                # Just a newline.
+                print()
+
+
+def unzip(zipped_path, quiet):
+    unzipped_path = "./libtorch/out" # os.path.splitext(zipped_path)[0]
+    if os.path.exists(unzipped_path):
+        if not quiet:
+            print("{} already exists, skipping ... ".format(unzipped_path))
+        return
+    # with gzip.open(zipped_path, "rb") as zipped_file:
+    #     with open(unzipped_path, "wb") as unzipped_file:
+    #         unzipped_file.write(zipped_file.read())
+    #         if not quiet:
+    #             print("Unzipped {} ...".format(zipped_path))
+    
+    with zipfile.ZipFile(zipped_path,"r") as zip_ref:
+        zip_ref.extractall(unzipped_path)
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Download the lib-torch")
+    parser.add_argument(
+        "-d", "--destination", default="./libtorch", help="Destination directory"
+    )
+    parser.add_argument(
+        "-q", "--quiet", action="store_true", help="Don't report about progress"
+    )
+    options = parser.parse_args()
+
+    if not os.path.exists(options.destination):
+        os.makedirs(options.destination)
+
+    try:
+        system  = platform.system()
+        sources = Resources[system]
+        item = 0
+        for source in sources:
+            path = os.path.join(options.destination, Items[system][item])
+            url = source
+            download(path, url, options.quiet)
+            unzip(path, options.quiet)
+            item += 1
+    except KeyboardInterrupt:
+        print("Interrupted")
+
+
+if __name__ == "__main__":
+    main()
